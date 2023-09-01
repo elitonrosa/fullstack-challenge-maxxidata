0c24788fedeb35b13d7c9923d356ccf3c8f9df05
Eliton da Rosa <elitondr@live.com>
2023-09-01 13:46:46 -0300

    chore: added docker-compose.yaml and Dockerfile for backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+
+const port = parseInt(process.env.PORT) || 3001;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
