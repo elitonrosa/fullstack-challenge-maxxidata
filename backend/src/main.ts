d806765206fd475feae4aafdeb29d1b7da8210b1
Eliton da Rosa <elitondr@live.com>
2023-09-11 14:06:44 -0300

    chore: update README.md

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,9 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as process from 'process';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory }                    from "@nestjs/core";
+import { AppModule }                      from "./app.module";
+import * as process                       from "process";
+import { ValidationPipe }                 from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,15 +14,13 @@ async function bootstrap() {
   });
   const config = new DocumentBuilder()
     .setTitle('Professional Management API')
-    .setDescription(
-      'API para gerenciamento de profissionais e tipos de profissionais.',
-    )
+    .setDescription('API para gerenciamento de profissionais e tipos de profissionais.')
     .setVersion('1.0')
     .addTag('Profissionais')
     .addTag('Tipos de Profissionais')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/documentation', app, document);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
